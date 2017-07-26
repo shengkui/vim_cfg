87623d02dfd4513edc72b1d75a23c73f867b7e44
shengkui.leng <shengkui.leng@advantech.com.cn>
2017-07-26 14:53:43 +0800

    add script to install vim config

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VIM_DIR="${HOME}/.vim"
+
+mkdir -p "${VIM_DIR}"/{bundle,tags,undo} || { echo "make .vim dir error"; exit 1; }
+cp -avf UltiSnips "${VIM_DIR}" || { echo "copy UltiSnips error"; exit 1; }
+cp -vf _vimrc "${HOME}"/.vimrc || { echo "copy .vimrc error"; exit 1; }
+#git clone https://github.com/VundleVim/Vundle.vim.git "${VIM_DIR}"/bundle/Vundle.vim
+curl -fLo "${VIM_DIR}"/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+    || { echo "intsll vim-plug error"; exit 1; }
